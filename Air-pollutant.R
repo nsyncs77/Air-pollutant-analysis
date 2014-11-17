9431929a3f1b9e483d8f2e195afba2d9f577b0e1
nsyncs77 <nsyncs7777@msn.com>
2014-11-17 18:38:25 +0800

    Create Air-pollutant.R

---
+++ Air-pollutant.R
@@ -0,0 +1,8 @@
+plot1:  
+  NEI <- readRDS("summarySCC_PM25.rds")
+  
+  yeargroup <- tapply(NEI$Emissions, NEI$year, sum)
+  group <- data.frame(total = yeargroup, year = c("1999", "2002", "2005", "2008"))
+  png(filename="plot1.png", width=480, height=480)
+  with(group, plot(year, total, ylab = "Total Year Emissions", xlab="Year"))
+  dev.off()
